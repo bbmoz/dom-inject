910dd44a649f563066e6fa8c33cf730b712434ac
Joe Lee <blueberrymozart@gmail.com>
2017-04-01 15:51:33 -0500

    Cleanup spacing.

--- example/src/Hello.js
+++ example/src/Hello.js
@@ -2,15 +2,15 @@ import dom from './dom'
 
 @dom('heading')
 class Hello {
-    constructor (text) {
-        this.$heading = Hello.$.heading
-        this._displayDomElements(text)
-    }
+  constructor (text) {
+    this.$heading = Hello.$.heading
+    this._displayDomElements(text)
+  }
 
-    _displayDomElements (text) {
-        this.$heading.innerHTML = text
-        this.$heading.style = 'display: block'
-    }
+  _displayDomElements (text) {
+    this.$heading.innerHTML = text
+    this.$heading.style = 'display: block'
+  }
 }
 
 export default Hello
